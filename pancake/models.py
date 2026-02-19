b300e31e732a7fce6498ced3fe6843937a54c173
Joshua Ahmath Hairston <hairs3ja@dukes.jmu.edu>
2026-02-19 14:00:34 -0500

    we think we finished the models

--- pancake/models.py
+++ pancake/models.py
@@ -5,31 +5,38 @@ from django.db import models
 # TODO: we need to add membership to words and letters, look at the many to many fields and add its own membership
 
 # One user has one waffle
-# One waffle has many words
+# One waffle has one of each word in a specific position
 # One word has many letters
 # Many letters are in many words
 
 class Waffle(models.Model):
-  top_row_word = 
-  middle_row_word = 
-  bottom_row_word = 
-  left_col_word = 
-  middle_col_word = 
-  right_col_word = 
-  
+  # think about on_delete
+  top_row_word = models.OneToOneField('Word', on_delete=models.DO_NOTHING)
+  middle_row_word = models.OneToOneField('Word', on_delete=models.DO_NOTHING)
+  bottom_row_word = models.OneToOneField('Word', on_delete=models.DO_NOTHING)
+  left_col_word = models.OneToOneField('Word', on_delete=models.DO_NOTHING)
+  middle_col_word = models.OneToOneField('Word', on_delete=models.DO_NOTHING)
+  right_col_word = models.OneToOneField('Word', on_delete=models.DO_NOTHING)
+  num_turns_left = models.IntegerField()
 
 class User(models.Model):
   waffle = models.OneToOneField(Waffle)
     
 
+
+# We need a foreign key for the many to one relationship between words and letters
+# Words can only have one letter per index, letters can be in multiple words
+# After much math we added foreign key to word
 class Word(models.Model):
   correct_spelling = models.CharField(max_length=5)
-  waffle = models.ForeignKey(Waffle, on_delete=models.CASCADE)
-  current_letters = models.JSONField(default=list)
+  letter1 = models.ForeignKey('Letter', on_delete=models.CASCADE)
+  letter2 = models.ForeignKey('Letter', on_delete=models.CASCADE)
+  letter3 = models.ForeignKey('Letter', on_delete=models.CASCADE)
+  letter4 = models.ForeignKey('Letter', on_delete=models.CASCADE)
+  letter5 = models.ForeignKey('Letter', on_delete=models.CASCADE)
 
 
 class Letter(models.Model):
   character = models.CharField(max_length=1)
   correct_place = models.BooleanField()
   in_word = models.BooleanField()
-  word = models.ForeignKey(Word, on_delete=models.CASCADE)
